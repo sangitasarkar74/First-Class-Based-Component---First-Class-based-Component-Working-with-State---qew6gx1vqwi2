0451b49a3cb359bb4a50458686a48f4a1e09a97e
sangitasarkar74 <sangitasarkar@gmail.com>
2022-12-23 10:44:57 +0530

    more changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,26 @@
 import React from 'react'
 import '../styles/App.css';
 
-class App extends git Component {
+class App extends React.Component {
 
-    constructor(props) {
-        super(props)
+    constructor() {
+        super()
         this.state = { name: "Sam Lee", enrollmentNo: "457885", age: 25 }
     }
 
-    handleBtn() {
-        this.setState({
-            age: this.state.age + 1
-        })
-    }
 
     render() {
+        const handleBtn = () => {
+            let changeState = this.state.age;
+            this.setState({
+                age: changeState + 1
+            })
+        }
         return (
             <>
                 <h1>{`Hello, my name is ${this.state.name}`}</h1>
                 <p>{`I am ${this.state.age} years old and my enrollment no is ${this.state.enrollmentNo}`}</p>
-                <button onClick={() => this.handleBtn()}>Submit</button>
+                <button onClick={handleBtn}>Submit</button>
 
             </>
         )
